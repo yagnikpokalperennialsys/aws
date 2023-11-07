70447520e03e453c3a59303d9c930dd8fab2cffb
yagnikpokalperennialsys <yagnik.pokal@perennialsys.com>
2023-11-07 11:56:11 +0530

    Added init function to follow the singletonen

--- lambda/article/main.go
+++ lambda/article/main.go
@@ -1,11 +1,31 @@
 package main
 
 import (
-	"pet/internal/routes"
+	"context"
+	"pet/internal/controller"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/gin-gonic/gin"
 )
 
+var ginLambda *ginadapter.GinLambda
+
+func init() {
+	r := gin.Default()
+	r.GET("/articles", controller.GetArticles)
+	r.POST("/articles/:id", controller.GetArticle)
+	ginLambda = ginadapter.New(r)
+}
+
+func handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	if ginLambda == nil {
+		panic("ginLambda should not be nil")
+	}
+	return ginLambda.ProxyWithContext(ctx, req)
+}
+
 func main() {
-	lambda.Start(routes.Routes)
+	lambda.Start(handler)
 }
